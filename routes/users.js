176f6b47181b081bfa3a58a632aecc25ac46c7ff
Trey <tmcdeane@gmail.com>
2021-05-13 11:48:39 -0400

    finished

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var _ = require('lodash');
-var logger = require('../lib/logger');
+var _ = require("lodash");
+var logger = require("../lib/logger");
 var log = logger();
 
-var users = require('../init_data.json').data;
+var users = require("../init_data.json").data;
 var curId = _.size(users);
 
 /* GET users listing. */
-router.get('/', function(req, res) {
+router.get("/", function (req, res) {
   res.json(_.toArray(users));
 });
 
 /* Create a new user */
-router.post('/', function(req, res) {
+router.post("/", function (req, res) {
   var user = req.body;
+  console.dir(user);
   user.id = curId++;
   if (!user.state) {
-    user.state = 'pending';
+    user.state = "pending";
   }
   users[user.id] = user;
-  log.info('Created user', user);
+  log.info("Created user", user);
+
   res.json(user);
 });
 
 /* Get a specific user by id */
-router.get('/:id', function(req, res, next) {
+router.get("/:id", function (req, res, next) {
   var user = users[req.params.id];
   if (!user) {
     return next();
@@ -34,24 +36,23 @@ router.get('/:id', function(req, res, next) {
 });
 
 /* Delete a user by id */
-router.delete('/:id', function(req, res) {
+router.delete("/:id", function (req, res) {
   var user = users[req.params.id];
   delete users[req.params.id];
   res.status(204);
-  log.info('Deleted user', user);
+  log.info("Deleted user", user);
   res.json(user);
 });
 
 /* Update a user by id */
-router.put('/:id', function(req, res, next) {
+router.put("/:id", function (req, res, next) {
   var user = req.body;
   if (user.id != req.params.id) {
-    return next(new Error('ID paramter does not match body'));
+    return next(new Error("ID paramter does not match body"));
   }
   users[user.id] = user;
-  log.info('Updating user', user);
+  log.info("Updating user", user);
   res.json(user);
 });
-
 
 module.exports = router;
